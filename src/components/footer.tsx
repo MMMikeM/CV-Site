3d92fdf9eb5a98f788b332bd9c662531c024a49c
MMMike <mikem.tba@gmail.com>
2021-04-11 10:15:40 +0200

    fix: :bug: Testing on mobile device

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,9 @@ import Icon from "../components/icon";
 
 const Footer = ({ footerIcons }) => {
   return (
-    <div className="bg-gray-800 shadow-lg md:block hidden">
+    <div className="bg-gray-800 shadow-lg md:block hidden fixed sm:static">
       {footerIcons.length ? (
-        <div className="h-12 flex responsive-container items-center gap-2 px-4  fixed sm:static bottom-0">
+        <div className="h-12 flex responsive-container items-center gap-2 px-4 sm:static bottom-0">
           <a href="https://www.linkedin.com/in/michael-murray-80b876136/">
             <Icon
               iconList={footerIcons}
