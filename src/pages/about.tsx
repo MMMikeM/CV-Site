3f541dac6536ff02c4fceb2c9ebc2515f72e7b0b
MMMike <mikem.tba@gmail.com>
2021-04-11 20:34:27 +0200

    feat: :speech_balloon: Revised About Me section, added project information on cards

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,22 +6,24 @@ const About = ({ elementRef }) => {
           <h2 className="h2 mb-4">About me</h2>
           <p className="pb-2 text-sm md:text-base xl:text-lg">
             I have always had a passion for technology, particularly when it
-            comes to improving human interactions.
+            comes to improving human interactions. Not much makes me as happy as
+            optimising performance, whether between server and host or keyboard
+            and chair. I'm highly fascinated by the entire front-end spectrum,
+            from saving clicks for the user to shaving milliseconds off of
+            requests.
           </p>
           <p className="pb-2 text-sm md:text-base xl:text-lg">
             I am a driven individual, my friends would describe me as high
-            energy and full of jokes. In my free time, I enjoy keeping fit and
+            energy and full of jokes. In my free time, I enjoy keeping fit,
             playing soccer and trying to learn something new every day;
-            ocassionally I spend some time practicing typing or questioning
+            occasionally, I spend some time practising typing or questioning
             keyboard ergonomics.
           </p>
           <p className="pb-2 text-sm md:text-base xl:text-lg">
-            I started my journey in development at the beginning of 2019
-          </p>
-          <p className="pb-2 text-sm md:text-base xl:text-lg">
-            Prior to the era of social-distancing, I enjoyed attending developer
-            meetups. 2020 did open many doors to attend online conferences and
-            teleconferenced courses.
+            I started my journey in development at the beginning of 2019. Prior
+            to the era of social-distancing, I enjoyed attending developer
+            meetups. 2020 opened many doors where I had opportunities to attend
+            online conferences and join live teleconferenced courses.
           </p>
         </div>
         <div className="h-72 md:h-auto w-full sm:w-1/2 xl:w-1/3 md:ml-8 mt-8 md:mt-0 mb-8 md:mb-0 flex-grow flex justify-center items-center">
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,55 +2,72 @@ import Card from "../components/card";
 
 const Projects = ({ elementRef, setActiveView }) => {
   return (
-    <div className="wrapper" ref={elementRef}>
-      <div className="w-full md:h-12"></div>
-      <h2 className="h2">Things I have done</h2>
-      <div
-        className="
-      w-full grid grid-cols-1 gap-4 place-items-stretch px-4
-      md:grid-cols-2 md:mb-12 md:px-24   
-      2xl:grid-cols-3"
-      >
-        <Card
-          title="Carbon Carwash"
-          classes="h-80 flex flex-grow flex-col justify-between min-w-[260px]"
-          onClick={() => {
-            setActiveView({ view: "aside", aside: "ccw" });
-          }}
+    <div className="wrapper pt-10" ref={elementRef}>
+      <div className="bg-gray-925 shadow-lg rounded-md py-8 md:py-12 md:px-12">
+        <h2 className="h2 md:mb-10 ml-4">Things I have done</h2>
+        <div
+          className="
+      w-full 
+      grid grid-cols-1 gap-x-4 gap-y-8 place-items-stretch px-4
+      md:grid-cols-2 md:mb-12 md:px-4 md:gap-y-4"
         >
-          <h3 className="h5 my-2">React & Bootstrap</h3>
-          <h4 className="mb-10 text-primary-200">July 2020</h4>
-        </Card>
-        <Card
-          title="Contention Ratio CLI tool"
-          classes="h-80 flex flex-grow flex-col justify-between min-w-[260px]"
-          onClick={() => {
-            setActiveView({ view: "aside", aside: "cli" });
-          }}
-        >
-          <h3 className="h5 my-2">NodeJS & Jest</h3>
-          <h4 className="mb-10 text-primary-200">February 2021</h4>
-        </Card>
-        <Card
-          title="Web scraper"
-          classes="h-80 flex flex-grow flex-col justify-between min-w-[260px]"
-          onClick={() => {
-            setActiveView({ view: "aside", aside: "scraper" });
-          }}
-        >
-          <h3 className="h5 my-2">NodeJS</h3>
-          <h4 className="mb-10 text-primary-200">June 2020</h4>
-        </Card>
-        <Card
-          title="Tailwind Datepicker"
-          classes="h-80 flex flex-grow flex-col justify-between min-w-[260px]"
-          onClick={() => {
-            setActiveView({ view: "aside", aside: "calendar" });
-          }}
-        >
-          <h3 className="h5 my-2">React & Tailwindcss</h3>
-          <h4 className="mb-10 text-primary-200">April 2021</h4>
-        </Card>
+          <Card
+            title="Carbon Carwash"
+            subtitle="React & Bootstrap"
+            classes="min-h-[240px] md:h-80 flex flex-grow flex-col justify-between min-w-[240px]"
+            onClick={() => {
+              setActiveView({ view: "aside", aside: "ccw" });
+            }}
+          >
+            <p className="text-gray-200">
+              Example paragraph text that I am going to use to describe each
+              project briefly
+            </p>
+            <h4 className="mb-4 text-primary-200">July 2020</h4>
+          </Card>
+          <Card
+            title="Contention Ratio CLI tool"
+            subtitle="NodeJS & Jest"
+            classes="min-h-[240px] md:h-80 flex flex-grow flex-col justify-between min-w-[240px]"
+            onClick={() => {
+              setActiveView({ view: "aside", aside: "cli" });
+            }}
+          >
+            <p className="text-gray-200">
+              Example paragraph text that I am going to use to describe each
+              project briefly
+            </p>
+            <h4 className="mb-4 text-primary-200">February 2021</h4>
+          </Card>
+          <Card
+            title="Web scraper"
+            subtitle="NodeJS"
+            classes="min-h-[240px] md:h-80 flex flex-grow flex-col justify-between min-w-[240px]"
+            onClick={() => {
+              setActiveView({ view: "aside", aside: "scraper" });
+            }}
+          >
+            <p className="text-gray-200">
+              Example paragraph text that I am going to use to describe each
+              project briefly
+            </p>
+            <h4 className="mb-4 text-primary-200">June 2020</h4>
+          </Card>
+          <Card
+            title="Tailwind Datepicker"
+            subtitle="React & Tailwindcss"
+            classes="min-h-[240px] md:h-80 flex flex-grow flex-col justify-between min-w-[240px]"
+            onClick={() => {
+              setActiveView({ view: "aside", aside: "calendar" });
+            }}
+          >
+            <p className="text-gray-200">
+              Example paragraph text that I am going to use to describe each
+              project briefly
+            </p>
+            <h4 className="mb-4 text-primary-200">April 2021</h4>
+          </Card>
+        </div>
       </div>
     </div>
   );
