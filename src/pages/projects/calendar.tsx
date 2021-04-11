9177bb815b5d6659ad965cf3ced3d6d4765a03ca
MMMike <mikem.tba@gmail.com>
2021-04-11 19:15:50 +0200

    feat: :speech_balloon: Added more projects

---
+++ src/pages/projects/calendar.tsx
@@ -0,0 +1,32 @@
+const Calendar = () => {
+  return (
+    <div className="flex flex-col justify-between">
+      <h2 className="h2 mb-4">Tailwind Datepicker</h2>
+      <h4 className="h4 mb-4 font-normal mono">
+        Period:
+        <span className="text-secondary-300">April 2021</span>
+      </h4>
+      <h3 className="h3 text-primary-200">Achievements</h3>
+      <ul className="ml-8 list-disc">
+        <li className="experience-item mt-4">
+          Assisted a junior developer with building a date picker component that
+          can be used with Tailwind Css
+        </li>
+        <li className="experience-item">
+          Made use of a headless calendar hook, useLilius to expedite the
+          process
+        </li>
+        <li className="experience-item">
+          Component is being used in an opensource project
+        </li>
+        <img
+          className="w-1/3"
+          src="https://raw.githubusercontent.com/MMMikeM/useLilius-tailwindcss-calendar/main/src.gif"
+          alt="Cool date picker gif"
+        />
+      </ul>
+    </div>
+  );
+};
+
+export default Calendar;
---
+++ src/pages/projects/scraper.tsx
@@ -0,0 +1,27 @@
+const Scraper = () => {
+  return (
+    <div className="flex flex-col justify-between">
+      <h2 className="h2 mb-4">Web Scraper</h2>
+      <h4 className="h4 mb-4 font-normal mono">
+        Period:
+        <span className="text-secondary-300">June 2020</span>
+      </h4>
+      <h3 className="h3 text-primary-200">Achievements</h3>
+      <ul className="ml-8 list-disc">
+        <li className="experience-item mt-4">
+          Used code to solve a real world problem I faced - 100 in-stock units
+          which sold out in ~90 seconds and accumulated 5000 preorders in 24h
+        </li>
+        <li className="experience-item">
+          Scraped a website to check for stock using Cron Jobs in WSL
+        </li>
+        <li className="experience-item">
+          Integrated with Pushbullet API to send notifications to all personal
+          devices
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Scraper;
